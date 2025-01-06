a8fc9b6f219f3a3229163b946144843dcd735d4f
jwiggi18 <jodiewiggins18@gmail.com>
2025-01-06 14:56:36 -0600

    year and teaching team updates

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-var myIndex = 0;
-carousel();
+var SlideList1 = ['img/wiggins_family.jpg', 'img/wiggins_dam2.jpg', 'img/wiggins_graduate.jpg', 'img/wiggins_labbench.jpg', 'img/wiggins_pibbles.jpg'];
+var SlideShow1 = new SlideShow(SlideList1, 'slide1', 5000, "SlideShow1");
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
-  setTimeout(carousel, 5000); // Change image every 5 seconds
+var SlideList2 = ['img/leone_loki.jpg', 'img/leone_xmas.jpg', 'img/leone_potter.jpg'];
+var SlideShow2 = new SlideShow(SlideList2, 'slide2', 5000, "SlideShow2");
+
+var SlideList3 = ['img/spresser_pupper1.jpg', 'img/spresser_pupper2.jpg', 'img/spresser_field.jpg'];
+var SlideShow3 = new SlideShow(SlideList3, 'slide3', 5000, "SlideShow3");
+
+function SlideShow(slideList, image, speed, name) {
+  this.slideList = slideList;
+  this.image = image;
+  this.speed = speed;
+  this.name = name;
+  this.current = 0;
+  this.timer = 0;
 }
+
+function switchImage(imgName, imgSrc) {
+  if (document.images) {
+    document.images[imgName].src = imgSrc;
+  }
+}
+
+SlideShow.prototype.play = function () {
+  with (this) {
+    if (current++ == slideList.length - 1) current = 0;
+    switchImage(image, slideList[current]);
+    clearTimeout(timer);
+    timer = setTimeout(name + '.play()', speed);
+  }
+};
+
+window.onload = function () {
+  SlideShow1.play();
+  SlideShow2.play();
+  SlideShow3.play();
+};
+
--- slides.js
+++ slides.js
@@ -2,14 +2,11 @@
  
 var SlideList1 = ['img/wiggins_family.jpg', 'img/wiggins_dam2.jpg', 'img/wiggins_graduate.jpg', 'img/wiggins_labbench.jpg', 'img/wiggins_pibbles.jpg'];
 var SlideShow1 = new SlideShow(SlideList1, 'slide1', 5000, "SlideShow1");
-var SlideList2 = ['img/leone_loki.jpg', 'img/leone_xmas.jpg', 'img/leone_potter.jpg'];
+var SlideList2 = ['img/desi1.png', 'img/desi2.png', 'img/desi3.png'];
 var SlideShow2 = new SlideShow(SlideList2, 'slide2', 5000, "SlideShow2");
-var SlideList3 = ['img/spresser_pupper1.jpg', 'img/spresser_pupper2.jpg', 'img/spresser_field.jpg'];
+var SlideList3 = ['img/claudia1.png', 'img/claudia2.png', 'img/claudia3.png'];
 var SlideShow3 = new SlideShow(SlideList3, 'slide3', 5000, "SlideShow3");
-var SlideList4 = ['img/hood_library.jpg', 'img/hood_medal.jpg', 'img/hood_rainbow.jpg'];
-var SlideShow4 = new SlideShow(SlideList4, 'slide4', 5000, "SlideShow4");
-var SlideList5 = ['img/hood_library.jpg', 'img/hood_medal.jpg', 'img/hood_rainbow.jpg'];
-var SlideShow5 = new SlideShow(SlideList5, 'slide5', 5000, "SlideShow5");
+
 
 function SlideShow(slideList, image, speed, name)
     {
@@ -44,4 +41,4 @@ SlideShow.prototype.play = SlideShow_play;
   }
 }
 
-window.onLoad = SlideShow1.play(); SlideShow2.play(); SlideShow3.play(); SlideShow5.play();
+window.onLoad = SlideShow1.play(); SlideShow2.play(); SlideShow3.play();
