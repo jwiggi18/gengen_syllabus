f909d7510516beb001a16dd70e0415d38dd59afe
jwiggi18 <jodiewiggins18@gmail.com>
2025-01-22 14:42:45 -0600

    add katriana info and pic

--- slides.js
+++ slides.js
@@ -6,6 +6,8 @@ var SlideList2 = ['img/desi1.jpg', 'img/desi2.jpg', 'img/desi3.jpg', 'img/desi4.
 var SlideShow2 = new SlideShow(SlideList2, 'slide2', 5000, "SlideShow2");
 var SlideList3 = ['img/claudia1.jpeg', 'img/claudia2.jpeg', 'img/claudia3.jpeg'];
 var SlideShow3 = new SlideShow(SlideList3, 'slide3', 5000, "SlideShow3");
+var SlideList4 = ['img/Mapesgeneticsphotos.png'];
+var SlideShow4 = new SlideShow(SlideList4, 'slide4', 5000, "SlideShow4");
 
 
 function SlideShow(slideList, image, speed, name)
@@ -41,4 +43,4 @@ SlideShow.prototype.play = SlideShow_play;
   }
 }
 
-window.onLoad = SlideShow1.play(); SlideShow2.play(); SlideShow3.play();
+window.onLoad = SlideShow1.play(); SlideShow2.play(); SlideShow3.play(); SlideShow4.play();
